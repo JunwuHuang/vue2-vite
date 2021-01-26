48ac65b0b6919f32f7143538ea40cf5eba692081
JunwuHuang <978162622@qq.com>
2021-01-27 02:00:10 +0800

    feat: initialize repository

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { createVuePlugin } from 'vite-plugin-vue2'
+import babel from '@rollup/plugin-babel'
+
+/**
+ * @type {import('vite').UserConfig}
+ */
+export default {
+  plugins: [
+    createVuePlugin({
+      jsx: true
+    }),
+    babel()
+  ]
+}
